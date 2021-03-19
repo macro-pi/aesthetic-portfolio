19e9728ecf29ce745aadb00c6af9d9371f6de1f6
Jeremiah Pitts <jeremiahnpitts@gmail.com>
2021-03-18 22:13:14 -0500

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*function flashingText(obj, arr, int){
+  setInterval(setText(obj, arr, int), int);
+  function setText(obj, arr, int){
+    for(int i = 0; i < arr.lengthnewFunction(); i++){
+      if(i = arr.length){
+        let i = 0;
+      }
+      document.getElementById(obj).innerHTML = arr[i];
+    }
+  }
+}
+*/
